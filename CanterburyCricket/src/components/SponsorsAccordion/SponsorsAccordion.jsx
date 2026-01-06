08ef2e4fc9514e134572e435d8b3cfc4fbb0e2c7
Sanjeev Paul <psanjeev@fortinet.com>
2026-01-06 14:22:09 -0500

    Pictured Accordion

--- CanterburyCricket/src/components/SponsorsAccordion/SponsorsAccordion.jsx
+++ CanterburyCricket/src/components/SponsorsAccordion/SponsorsAccordion.jsx
@@ -1,38 +1,46 @@
 import { useState } from "react";
+import image1 from "./1.jpg";
+import image2 from "./2.jpg";
+import image3 from "./3.jpg";
+import image4 from "./4.jpg";
+import image5 from "./5.jpg";
 import "./SponsorsAccordion.module.css";
 
-export const Accordion1 = ({ items }) => {
+const cards = [
+  { header: "Canada", image: image2, text: `Image description here` },
+  { header: "Bali", image: image1, text: `Image description here` },
+  { header: "Spain", image: image3, text: `Image description here` },
+  { header: "Indonesia", image: image4, text: `Image description here` },
+  { header: "South Africa", image: image5, text: `Image description here` },
+];
+
+export const Sponsors = () => {
   const [active, setActive] = useState(0);
 
+  // Remove ": number" type annotation
   const handleToggle = (index) => setActive(index);
 
   return (
-    <section className=" page image-accordion-page">
-      <div
-        className="bg"
-        style={{ backgroundImage: `url(${items[active].image}` }}
-      />
-      <div className="image-accordion">
-        {items.map((item, index) => {
-          const isActive = active === index ? "active" : "";
-          return (
-            <div
-              key={item.image}
-              className={`image-accordion-item ${isActive}`}
-              onClick={() => handleToggle(index)}
-            >
-              <img src={item.image} />
-              <div className="content">
-                <span className="material-symbols-outlined">photo_camera</span>
-                <div>
-                  <h2>{item.header}</h2>
-                  <p>{item.text}</p>
-                </div>
+    <section>
+      {cards.map((card, index) => {
+        const isActive = active === index ? "active" : "";
+        return (
+          <article
+            key={card.image}
+            className={isActive}
+            onClick={() => handleToggle(index)}
+          >
+            <img src={card.image} alt={card.header} />
+            <div className="content">
+              <span className="material-symbols-outlined">photo_camera</span>
+              <div>
+                <h2>{card.header}</h2>
+                <p>{card.text}</p>
               </div>
             </div>
-          );
-        })}
-      </div>
+          </article>
+        );
+      })}
     </section>
   );
 };
