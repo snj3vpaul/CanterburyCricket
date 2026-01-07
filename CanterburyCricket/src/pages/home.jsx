9853017c86777ce050606ce2ab61bd64af8e3410
Sanjeev Paul <psanjeev@fortinet.com>
2026-01-07 12:55:53 -0500

    Typo under home.jsx

--- CanterburyCricket/src/pages/home.jsx
+++ CanterburyCricket/src/pages/home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 //import VideoHero from "../components/VideoHero/VideoHero";//
-import {Parallax1} from "../components/parallax/parallax1"; 
+import {Parallax1} from "../components/parallax/Parallax1"; 
 //import ParallaxVideo from "../components/parallax/ParallaxVideo";//
 export default function Home() {
   return (
