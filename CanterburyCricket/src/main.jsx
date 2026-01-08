e2bf8f2dd41ccdb3ca1bf84d12038de8ad978642
Rkaur2007 <rupinder88uiet@gmail.com>
2026-01-08 00:06:20 -0500

    Changes to Main for Vercel

--- CanterburyCricket/src/main.jsx
+++ CanterburyCricket/src/main.jsx
@@ -3,8 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-// NextUI
-import { NextUIProvider } from "@nextui-org/react";
+
 
 // MUI
 import { ThemeProvider, createTheme } from "@mui/material/styles";
