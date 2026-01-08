7e6a8c0eaac3f17dfde34ea27052f9588088c04b
Rkaur2007 <rupinder88uiet@gmail.com>
2026-01-07 23:56:39 -0500

    2024 Stats

--- CanterburyCricket/src/pages/Season.jsx
+++ CanterburyCricket/src/pages/Season.jsx
@@ -5,7 +5,7 @@ import { Card1 } from "../components/FlipCards/overflipcards";
 export default function Season() {
   return (
     <div className="season-page">
-      <h1>Season Highlights</h1>
+      
       <SeasonTable />
     </div>
   );
